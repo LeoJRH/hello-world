c30e16cc9354dcb7c7bbda3c7d12e9dfc6b21d29
LeoJRH <hzstudy@qq.com>
2019-07-08 22:05:04 +0800

    sec

--- irias.py
+++ irias.py
@@ -1,8 +1,13 @@
 from sklearn.datasets import load_iris, fetch_20newsgroups
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
-li = load_iris()
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+# li = load_iris()
 
 #包含K近邻算法、贝叶斯算法等实现
 
@@ -20,36 +25,83 @@ li = load_iris()
 # x_train,x_test,y_train,y_test = train_test_split(li.data, li.target, test_size=0.25)
 #
 # print("Train x and y:", x_train, y_train)
-# print("Test x and y:", x_test, y_test)
+# # print("Test x and y:", x_test, y_test)
+#
+# # news = fetch_20newsgroups(subset='all')
+# # print(news.data)
+# # print(news.target)
+#
+# def naviebayes():
+#     news = fetch_20newsgroups(subset='all')
+#
+#     #Data split
+#     x_train,x_test,y_train,y_test = train_test_split(news.data, news.target,test_size=0.25)
+#
+#     # feature extraction
+#     tf = TfidfVectorizer()
+#     #Use x_train to make statistics of each arctics 's importance
+#     x_train = tf.fit_transform(x_train)
+#     print(tf.get_feature_names())
+#     x_test = tf.transform(x_test)
+#     # bATES calculate
+#
+#     mlt = MultinomialNB(alpha=1.0)
+#     print(x_train)
+#     mlt.fit(x_train, y_train)
+#
+#     y_predict = mlt.predict(x_test)
+#     #Score
+#     print("Predict kind is", y_predict)
+#     print("The score is ",mlt.score(x_test,y_test))
+#
+#     return None
 
-# news = fetch_20newsgroups(subset='all')
-# print(news.data)
-# print(news.target)
 
-def naviebayes():
-    news = fetch_20newsgroups(subset='all')
+def decision():
+    tit = pd.read_csv('./ti.txt')
+    x = tit[['pclass','age','sex']]
+    y = tit['survived']
 
-    #Data split
-    x_train,x_test,y_train,y_test = train_test_split(news.data, news.target,test_size=0.25)
+    x['age'].fillna(x['age'].mean(),inplace=True)
 
-    # feature extraction
-    tf = TfidfVectorizer()
-    #Use x_train to make statistics of each arctics 's importance
-    x_train = tf.fit_transform(x_train)
-    print(tf.get_feature_names())
-    x_test = tf.transform(x_test)
-    # bATES calculate
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25)
 
-    mlt = MultinomialNB(alpha=1.0)
-    print(x_train)
-    mlt.fit(x_train, y_train)
+    dt = DictVectorizer(sparse=False)
+    print(x_train.to_dict(orient= "records"))
 
-    y_predict = mlt.predict(x_test)
-    #Score
-    print("Predict kind is", y_predict)
-    print("The score is ",mlt.score(x_test,y_test))
+    x_train = dt.fit_transform(x_train.to_dict(orient= "records"))
+    x_test = dt.fit_transform(x_test.to_dict(orient= "records"))
 
-    return None
+    # dtc = DecisionTreeClassifier()
+    # dtc.fit(x_train,y_train)
+    # dt_predict = dtc.predict(x_test)
+    rf = RandomForestClassifier()
+
+    param = {"n_estimators":[120,300,500,800,1200],"max_depth":[5,8,15,25,30]}
+
+    gc = GridSearchCV(rf,param_grid=param,cv=2)
+
+    gc.fit(x_train,y_train)
+
+    print("CCCCC Score:",gc.score(x_test,y_test))
+
+    print("CHoose Model:",gc.best_params_)
+
+
+    # print(dtc.score(x_test,y_test))
+    #print(classification_report(y_test, dt_predict, target_names=["died", "survived"]))
+
+
+
+
+
+
+
+
+
+    pass
+
 
 if __name__ == '__main__':
-    naviebayes()
+
+    decision()
